d70a0421684f82657fcb3a075e46e8a0b2b3c4e4
Natsuo Yamamoto <n-yamamoto@nifty.com>
2015-07-28 17:40:04 +0900

    fix typo

--- cal_lunar.js
+++ cal_lunar.js
@@ -1,29 +1,28 @@
 function Lunar(objDate) {
+	var i, leap=0, temp=0;
+	var offset   = (Date.UTC(objDate.getFullYear(),objDate.getMonth(),objDate.getDate()) - Date.UTC(1900,0,31))/86400000;
 
-var i, leap=0, temp=0;
-var offset   = (Date.UTC(objDate.getFullYear(),objDate.getMonth(),objDate.getDate()) - Date.UTC(1900,0,31))/86400000;
+	for(i=1900; i<2050 && offset>0; i++) { temp=lYearDays(i); offset-=temp; }
 
-for(i=1900; i<2050 && offset>0; i++) { temp=lYearDays(i); offset-=temp; }
+	if(offset<0) { offset+=temp; i--; }
 
-if(offset<0) { offset+=temp; i--; }
+	this.year = i;
 
-this.year = i;
+	leap = leapMonth(i); //
+	this.isLeap = false;
+    monthDays(:
+	for(i=1; i<13 && offset>0; i++) {
+		//
+		if(leap>0 && i==(leap+1) && this.isLeap==false)
+		{ --i; this.isLeap = true; temp = leapDays(this.year); }
+		else
+		{ temp = monthDays(this.year, i); }
 
-leap = leapMonth(i); //
-this.isLeap = false;
+		//
+		if(this.isLeap==true && i==(leap+1)) this.isLeap = false;
 
-for(i=1; i<13 && offset>0; i++) {
-//
-if(leap>0 && i==(leap+1) && this.isLeap==false)
-{ --i; this.isLeap = true; temp = leapDays(this.year); }
-else
-{ temp = monthDays(this.year, i); }
-
-//
-if(this.isLeap==true && i==(leap+1)) this.isLeap = false;
-
-offset -= temp;
-}
+		offset -= temp;
+	}
 
 if(offset==0 && leap>0 && i==leap+1)
 if(this.isLeap)
